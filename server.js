4b315c565e7625af48987556aea9d18da7f4262d
chaithanya <krishnachaithanya826@gmail.com>
2023-09-30 17:25:23 +0530

    updated to https

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require("dotenv").config()
 const WebSocketServer = require("ws").WebSocketServer
 const WebSocket = require("ws")
-const createServer = require("http").createServer
+const createServer = require("https").createServer
 const PORT = process.env.PORT || 9847
 const server = createServer()
 const wss = new WebSocketServer({ server })
 
 wss.on("connection", ws => {
-    ws.on("error", err => console.error(err))
+    ws.on("error", console.error)
     ws.on("message", data => {
         wss.clients.forEach(client => {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
