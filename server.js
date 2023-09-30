d693f9429c40f379798995e8d2b930df2a0fc3f4
chaithanya <krishnachaithanya826@gmail.com>
2023-09-30 17:07:22 +0530

    added dotenv

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
+require("dotenv").config()
 const WebSocketServer = require("ws").WebSocketServer
 const WebSocket = require("ws")
 const createServer = require("http").createServer
-const PORT = 8080
-
+const PORT = process.env.PORT || 9847
 const server = createServer()
 const wss = new WebSocketServer({ server })
 
-wss.on("connection", function connection(ws) {
-    ws.on("error", function errormsg(err) {
-        console.error(err)
-    })
-    ws.on("message", function message(data) {
-        wss.clients.forEach(function each(client) {
+wss.on("connection", ws => {
+    ws.on("error", err => console.error(err))
+    ws.on("message", data => {
+        wss.clients.forEach(client => {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(data.toString())
             }
         })
     })
 })
-
 server.listen(PORT, () => console.log(`Server running on port:${PORT}`))
