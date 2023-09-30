f62f35810ff7b92e9d9206c1f327e075845f591b
chaithanya <krishnachaithanya826@gmail.com>
2023-09-30 16:31:30 +0530

    added error log

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const server = createServer()
 const wss = new WebSocketServer({ server })
 
 wss.on("connection", function connection(ws) {
-    ws.on("error", console.error)
+    ws.on("error", function errormsg(err) {
+        console.error(err)
+    })
     ws.on("message", function message(data) {
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
